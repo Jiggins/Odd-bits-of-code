4d7e88e3f800e9c5fa367424f8fdc461c2951353
Jack Higgins <jackhiggins07@gmail.com>
2013-05-08 18:50:20 +0100

    Added Conor's suggestion

--- Integration.hs
+++ Integration.hs
@@ -1,17 +1,19 @@
-trapesiumRule f xs = h * (sum $ f a : f b : map (*2) [f x | x <- slice 2 (length xs - 1) xs])
+trapesiumRule f xs = h * (sum $ f a : f b : map (*2) [f x | x <- init $ tail xs])
   where a = head xs
         b = last xs
         n = fromIntegral $ length xs
         h = (b - a) / (2 * n)
 
+<<<<<<< HEAD
 simpsonsRule f xs = (h / 3) * (sum $ f a : f b : (map (*2) [x | x <- filter odd $ slice 2 (length xs - 1) xs] ++ (map (*4) [x | x <- filter odd $ slice 2 (length xs - 1) xs])))
+=======
+
+simpsonsRule f xs = (h / 3) * (sum $ f a : f b : (map (*2) [x | x <- filter odd $ init $ tail xs] ++ (map (*2) [x | x <- filter odd $ init $ tail xs])))
+>>>>>>> Added Conor's suggestion
  where a = head xs
        b = last xs
        n = fromIntegral $ length xs
        h = (b - a) / (2 * n)
-
-slice :: Int -> Int -> [a] -> [a]
-slice from to xs = take (to - from + 1) (drop (from - 1) xs)
 
 main = do
 	let f x = x^2 + x + 1
