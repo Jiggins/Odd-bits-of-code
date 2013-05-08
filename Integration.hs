b568dc87c7a67d6cbcf951a3f6c06e7d7b92fcb0
Jack Higgins <jackhiggins07@gmail.com>
2013-05-08 19:25:54 +0100

    fixed based on Conor's suggestions

--- Integration.hs
+++ Integration.hs
@@ -1,15 +1,12 @@
+trapesiumRule :: (Double -> Double) -> [Double] -> Double
 trapesiumRule f xs = h * (sum $ f a : f b : map (*2) [f x | x <- init $ tail xs])
   where a = head xs
         b = last xs
         n = fromIntegral $ length xs
         h = (b - a) / (2 * n)
 
-<<<<<<< HEAD
-simpsonsRule f xs = (h / 3) * (sum $ f a : f b : (map (*2) [x | x <- filter odd $ slice 2 (length xs - 1) xs] ++ (map (*4) [x | x <- filter odd $ slice 2 (length xs - 1) xs])))
-=======
-
-simpsonsRule f xs = (h / 3) * (sum $ f a : f b : (map (*2) [x | x <- filter odd $ init $ tail xs] ++ (map (*2) [x | x <- filter odd $ init $ tail xs])))
->>>>>>> Added Conor's suggestion
+simpsonsRule :: (Double -> Double) -> [Double] -> Double
+simpsonsRule f xs = (h / 3) * (sum $ f a : f b : (map (*2) [f x | x <- init (tail xs)] ++ (map (*2) [f x | x <- init $ tail xs])))
  where a = head xs
        b = last xs
        n = fromIntegral $ length xs
@@ -17,4 +14,4 @@ simpsonsRule f xs = (h / 3) * (sum $ f a : f b : (map (*2) [x | x <- filter odd 
 
 main = do
 	let f x = x^2 + x + 1
-	print $ trapesiumRule f [1..5]
+	print $ simpsonsRule f [1..5]
