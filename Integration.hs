db15747c16a504832544ad4923de0d01e098ee5e
Jack Higgins <jackhiggins07@gmail.com>
2013-05-08 19:55:55 +0100

    Added seconds function in the Simpson's rule
    Seems to work, matches up with what I had on paper.

--- Integration.hs
+++ Integration.hs
@@ -6,11 +6,14 @@ trapesiumRule f xs = h * (sum $ f a : f b : map (*2) [f x | x <- init $ tail xs]
         h = (b - a) / (2 * n)
 
 simpsonsRule :: (Double -> Double) -> [Double] -> Double
-simpsonsRule f xs = (h / 3) * (sum $ f a : f b : (map (*2) [f x | x <- init (tail xs)] ++ (map (*2) [f x | x <- init $ tail xs])))
+simpsonsRule f xs = (h / 3) * (sum $ f a : f b : (map (*2) [f x | x <- seconds $ init xs] ++ (map (*4) [f x | x <- seconds $init $ tail xs])))
  where a = head xs
        b = last xs
        n = fromIntegral $ length xs
        h = (b - a) / (2 * n)
+       seconds [] = []
+       seconds (_:[]) = []
+       seconds (x:y:xs) = y : seconds xs
 
 main = do
 	let f x = x^2 + x + 1
