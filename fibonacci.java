515598f21fccb5faac8b222479f65b759b116a9c
Jack Higgins <jackhiggins07@gmail.com>
2013-05-22 19:20:15 +0100

    int

--- fibonacci.java
+++ fibonacci.java
@@ -12,7 +12,7 @@ public static void fibonacci(int a, int b, int limit) {
 }
 
 //My way
-public static fibonacci(int n) {
+public static int fibonacci(int n) {
 	if (n == 0) return 0;	//Early return statements, here we are giving the first two fibonacci numbers
 	if (n == 1) return 1;	//so the program knows where to end.
 	return fibonacci(n-1) + fibonacci(n-2));
