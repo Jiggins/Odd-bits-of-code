998ef2fec5f4d8a0e0a2f64d559a4dd8b1c283af
Jack Higgins <jackhiggins07@gmail.com>
2013-05-22 19:18:37 +0100

    Comments

--- fibonacci.java
+++ fibonacci.java
@@ -7,13 +7,13 @@ public static void fibonacci(int a, int b, int limit) {
 		int sum = a + b;
 		limit = limit - 1;
 		Sysytem.out.println(sum + " ");
-		fibonacci(b, sum, limit); //That should be return fibonacci... but thats his notes.
+		fibonacci(b, sum, limit);	//That should be return fibonacci... but thats from his notes.
 	}
 }
 
 //My way
 public static fibonacci(int n) {
-	if (n == 0) return 0;
-	if (n == 1) return 1;
+	if (n == 0) return 0;	//Early return statements, here we are giving the first two fibonacci numbers
+	if (n == 1) return 1;	//so the program knows where to end.
 	return fibonacci(n-1) + fibonacci(n-2))
 }
