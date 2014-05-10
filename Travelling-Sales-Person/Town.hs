911fb631cc19bdb5e839b6f657bf04538cafea2c
Jack Higgins <jackhiggins07@gmail.com>
2014-05-10 23:53:10 +0100

    Fixed Indentation

--- Travelling-Sales-Person/Town.hs
+++ Travelling-Sales-Person/Town.hs
@@ -1,37 +1,37 @@
 module Town where 
-	import Data.Array
-	import Data.List
-	import Haversine
+    import Data.Array
+    import Data.List
+    import Haversine
 
-	type TownId = Int
+    type TownId = Int
 
 
-	data Town = Town { townId :: Int
-					  ,name  :: String
-					  ,coords :: (Double, Double)
-					  ,visited :: Bool
-					 }
+    data Town = Town { townId :: Int
+                      ,name  :: String
+                      ,coords :: (Double, Double)
+                      ,visited :: Bool
+                     }
 
-	instance Show Town where
-		show town = intercalate " " [show (townId town)
-		                            ,name town
-		                            ,show (coords town)]
+    instance Show Town where
+        show town = intercalate " " [show (townId town)
+                                    ,name town
+                                    ,show (coords town)]
 
-	{- Path type, implemented for storing a best path and nice printing -}
-	data Path = Path {path :: [Int], distance :: Double}
+    {- Path type, implemented for storing a best path and nice printing -}
+    data Path = Path {path :: [Int], distance :: Double}
 
-	instance Show Path where
-		show p = concat ["Path:\t\t", (intercalate "." . map show . path) p, 
-			             "\nDistance:\t", show (distance p)]
+    instance Show Path where
+        show p = concat ["Path:\t\t", (intercalate "." . map show . path) p, 
+                         "\nDistance:\t", show (distance p)]
 
-	getTown :: TownId -> Array Int Town -> Town
-	getTown town = (! town)
+    getTown :: TownId -> Array Int Town -> Town
+    getTown town = (! town)
 
-	getCoords :: Ix i => i -> Array i Town -> Coords
-	getCoords town = coords . (! town)
+    getCoords :: Ix i => i -> Array i Town -> Coords
+    getCoords town = coords . (! town)
 
-	visit :: Town -> Town
-	visit town = Town (townId town) (name town) (coords town) True
+    visit :: Town -> Town
+    visit town = Town (townId town) (name town) (coords town) True
 
-	wasVisited :: Town -> Bool
-	wasVisited = visited
+    wasVisited :: Town -> Bool
+    wasVisited = visited
